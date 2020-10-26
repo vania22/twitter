165846389501fd7037d14c3a0334af30506ae3fd
Ivan Krupskyi <krupskiy111@gmail.com>
2020-10-26 15:47:33 +0000

    added material-ui and custom material-ui theme

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,17 @@
+import { ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
 
 import './index.css';
+import theme from './theme';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
     </React.StrictMode>,
     document.getElementById('root'),
 );
